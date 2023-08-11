b59a8fa72055ba845cb3268fa8e2a1116bb7866d
tetraloba <four.leaves.siva7342@gmail.com>
2023-08-11 23:45:45 +0900

    add_playlist_table

--- src/player.php
+++ src/player.php
@@ -11,6 +11,34 @@ open_html("Player");
     }
 </style>
 
+<?php
+$playlist = array();
+$playlist_filename = $_GET['playlist'];
+$playlist_name = pathinfo($playlist_filename, PATHINFO_FILENAME);
+$playlist_file = fopen("./playlists/{$playlist_filename}", "r"); // プレイリストディレクトリのハードコーディングは気になるところ
+if (!$playlist_file) {
+    html_exit("プレイリスト {$playlist_filename} を開けませんでした。");
+}
+while ($audio = trim(fgets($playlist_file))) {
+    array_push($playlist, $audio);
+}
+?>
+<br id="playlist_name"><?=$playlist_name?></br>
+<form id="playlist_audio_file_list">
+<table border="1">
+<?php
+tbl_line(['', 'プレイリスト']);
+foreach ($playlist as $audio) {
+    tbl_line([
+        '<input type="radio" name="playing" value="'.$audio.'"><br>',
+        $audio,
+    ]);
+}
+?>
+</table><br>
+</form>
+
+<!-- メディアプレイヤー実体 -->
 <div id="audio_container">
     <audio id="player" controls src="path_to_audio_file" type="audio/flac" >title</audio>
 </div>
@@ -18,12 +46,9 @@ open_html("Player");
 <script>
     let playlist = [
 <?php
-$playlist = fopen("./playlists/{$_GET['playlist']}", "r");
-if (!$playlist) {
-    html_exit("プレイリスト {$_GET['playlist']} を開けませんでした。");
-}
-while ($audio_file = trim(fgets($playlist))) {
-    echo '"./music/'.$audio_file.'",'."\n";
+/* プレイリストを出力 */
+foreach ($playlist as $audio) {
+    echo '"./music/'.$audio.'",'."\n";
 }
 ?>
     ]
