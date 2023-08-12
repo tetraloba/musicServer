a68ea90889add0f8c2afa0dd565cf887e3474de8
tetraloba <four.leaves.siva7342@gmail.com>
2023-08-12 12:55:08 +0900

    create_custom_audio_player

--- src/player.php
+++ src/player.php
@@ -41,38 +41,152 @@ foreach ($playlist as $audio) {
 </table><br>
 </form>
 
-<!-- メディアプレイヤー実体 -->
+<!-- メディアプレイヤ実体 -->
 <div id="audio_container">
-    <audio id="player" controls src="path_to_audio_file" type="audio/flac" >title</audio>
+    <audio id="player" controls src="path_to_audio_file" type="audio/flac" >title</audio><br>
+    <button id="prev">prev</button>
+    <button id="stop">stop</button>
+    <button id="play_pause">play</button>
+    <button id="next">next</button><br>
+    <span id="current_time" class="time">00:00</span>
+    <input type="range" id="seekbar" max="100" value="0" />
+    <span id="duration" class="time">00:00</span>
+    <input type="range" id="volumebar" max="100" value="100" /> <!-- 毎回デフォルト100で良いのかな？ -->
+    <span id="volume">100%</span>
 </div>
 
 <script>
-    let playlist = [
+/* プレイリストの再生とループ */
+let playlist = [
 <?php
 /* プレイリストを出力 */
 foreach ($playlist as $audio) {
     echo '"./music/'.$audio.'",'."\n";
 }
 ?>
-    ]
+]
     
-    let audio_container = document.getElementById("audio_container");
-    let audio = document.getElementById('player');
+let audio_container = document.getElementById("audio_container");
+let audio = document.getElementById('player');
 
-    audio.src = playlist[0];
-    // audio.play(); // 最初はユーザの操作によってしか再生できない。
-    let index = 0;
-    audio.addEventListener('ended', function(){
-        index++;
-        if (index < playlist.length) {
-            audio.src = playlist[index];
-            audio.play();
-        } else {
-            audio.src = playlist[0];
-            audio.play();
-            index = 0;
-        }
+audio.src = playlist[0];
+// audio.play(); // 最初はユーザの操作によってしか再生できない。
+let index = 0;
+audio.addEventListener('ended', () => {
+    index++;
+    if (index < playlist.length) {
+        audio.src = playlist[index];
+        audio.play();
+    } else {
+        audio.src = playlist[0];
+        audio.play();
+        index = 0;
+    }
+});
+
+/* プレイヤの操作 */
+/* play pause */
+const playPauseButton = document.getElementById('play_pause');
+let playing = false;
+playPauseButton.addEventListener('click', () => {
+    if (playing) {
+        audio.pause();
+        playPauseButton.textContent = "play";
+        playing = false;
+    } else {
+        audio.play();
+        playPauseButton.textContent = "pause";
+        playing = true;
+    }
+});
+/* stop */
+const stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => {
+    audio.pause();
+    audio.currentTime = 0;
+    playPauseButton.textContent = "play";
+    playing = false;
+});
+/* prev */
+const prevButton = document.getElementById('prev');
+prevButton.addEventListener('click', () => {
+    if (0 < index) {
+        index--;
+        audio.src = playlist[index];
+    }
+    audio.currentTime = 0;
+    audio.play();
+    playPauseButton.textContent = "pause";
+    playing = true;
+});
+/* next */
+const nextButton = document.getElementById('next');
+nextButton.addEventListener('click', () => {
+    index++;
+    if (index < playlist.length) {
+        audio.src = playlist[index];
+        audio.play();
+    } else {
+        audio.src = playlist[0];
+        audio.play();
+        index = 0;
+    }
+    playPauseButton.textContent = "pause";
+    playing = true;
+});
+
+/* プレイヤのシークバー等 */
+/* seekbar */
+const seekbar = document.getElementById('seekbar');
+if (audio.readyState) { // この辺り繰り返しが多いのでイベント側に揃えるのもありかも？
+    seekbar.max = audio.duration;
+} else {
+    audio.addEventListener('loadedmetadata', () => {
+        seekbar.max = audio.duration;
     });
+}
+seekbar.addEventListener('input', () => { // input を change にすると離したときに反映
+    audio.currentTime = seekbar.value;
+});
+audio.addEventListener('timeupdate', () => {
+    seekbar.value = audio.currentTime;
+});
+/* current_time */
+const hhmmssTime = (secs) => {
+    const hours = Math.floor(secs / 3600);
+    const minutes = Math.floor(secs / 60 % 60);
+    const seconds = Math.floor(secs % 60);
+    const hh = 0 < hours ? (hours < 10 ? `0${hours}:` : `${hours}:`) : '';
+    const mm = minutes < 10 ? `0${minutes}:` : `${minutes}:`;
+    const ss = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${hh}${mm}${ss}`;
+}
+const currentTime = document.getElementById('current_time');
+seekbar.addEventListener('input', () => {
+    currentTime.textContent = hhmmssTime(seekbar.value);
+});
+audio.addEventListener('timeupdate', () => {
+    currentTime.textContent = hhmmssTime(audio.currentTime);
+});
+/* duration */
+const duration = document.getElementById('duration');
+if (audio.readyState) {
+    duration.textContent = hhmmssTime(audio.duration);
+} else {
+    audio.addEventListener('loadedmetadata', () => {
+        duration.textContent = hhmmssTime(audio.duration);
+    });
+}
+/* volumebar */
+const volumebar = document.getElementById('volumebar');
+volumebar.addEventListener('input', () => {
+    audio.volume = volumebar.value / 100; // 0 ~ 1
+});
+/* volume */
+const volume = document.getElementById('volume');
+volumebar.addEventListener('input', () => {
+    volume.textContent = `${volumebar.value}%`;
+});
 </script>
 
 <?php
